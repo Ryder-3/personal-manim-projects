9b40d618d13587051726f080924d7e964751d304
Ryder <thefifthcamel@gmail.com>
2026-05-16 14:23:18 -0700

    made work on both

--- Advanced-calc-final/presentation.py
+++ Advanced-calc-final/presentation.py
@@ -1,13 +1,18 @@
+import numpy as np
+
 from manimlib import *
+
 
 class main(ThreeDScene):
     def construct(self) -> None:
+        frame = self.camera.frame
+
         #Types of integration
             #IBP
             #Partial Fractions
             #Improper Integrals
         #Average Value
-        self.camera.frame.set_euler_angles(0,0,0)
+        frame.set_euler_angles(0,0,0)
         
         #Integration in 3d
         part_3_introduction = VGroup()
@@ -45,20 +50,71 @@ class main(ThreeDScene):
 
         text = Text("Like this")
         text.shift(UP * 3)
+        text.fix_in_frame()
         self.play(Write(text))
 
-        RoS_example = VGroup3D()
+        self.play(
+            self.frame.animate.scale(1.85),
+            run_time=1.25,
+        )
 
-        graph = ThreeDAxes()
+        graph = ThreeDAxes(
+            x_range=(-1, 10, 1),
+            y_range=(-1, 10, 1),
+            z_range=(-5, 5, 1),
+        )
+        graph.add_coordinate_labels()
 
-        circle = Circle()
-        circle.shift(UR * 2)
+        # Segment of y = x in the xy-plane (ThreeDAxes.get_graph is z = f(u, v), not a plane curve)
+        x_a, x_b = 0, 6.0
+        y_eq_x = ParametricCurve(
+            lambda t: graph.c2p(t, t, 0),
+            t_range=(x_a, x_b, 0.05),
+            color=YELLOW,
+        )
+        y_eq_x_label = Tex(r"y=x\text{. From }0\le x\le 6", color = YELLOW)
 
-        
+        self.add(graph)
+        self.play(ShowCreation(y_eq_x), FadeIn(y_eq_x_label))
+        self.wait()
+        self.wait()
 
-        self.play(ShowCreation(graph))
-        self.play(ShowCreation(circle))
-        
+        # Surface from revolving y = x about the x-axis: (x, y, z) = (u, u cos v, u sin v) in axis coords
+        cone_of_revolution = ParametricSurface(
+            lambda u, v: graph.c2p(u, u * np.cos(v), u * np.sin(v)),
+            u_range=(x_a, x_b),
+            v_range=(0, TAU),
+            resolution=(31, 49),
+            color=BLUE_E,
+            opacity=0.85,
+        )
+        cone_mesh = SurfaceMesh(cone_of_revolution)
+
+        revolving_shell = cone_of_revolution.copy()
+        revolving_shell.pointwise_become_partial(cone_of_revolution, 0, 0, axis=1)
+
+        self.play(
+            UpdateFromAlphaFunc(
+                revolving_shell,
+                lambda m, a: m.pointwise_become_partial(cone_of_revolution, 0, a, axis=1),
+            ),
+            y_eq_x.animate.set_stroke(opacity=0.25),
+            self.frame.animate.set_euler_angles(
+                theta=0.46691254,
+                phi=0.75668624,
+                gamma=0.34906585,
+            ),
+            run_time=4,
+        )
+        self.add(cone_mesh)
+        self.play(ShowCreation(cone_mesh, lag_ratio=0.01), run_time=2)
+
+        volume_find_text = VGroup()
+        text_1 = Text("To find the volume of this, we do something very similar to integration")
+        text_2 = Text("We slice the solid into a bunch of cylendars.")
+        text_3 = Text("First, we make a cylendar with a width of ")
+
+        self.embed()
+
 
             #Volumes of known cross sections
-        return super().construct()
--- stats-final-project/project-animation.py
+++ stats-final-project/project-animation.py
@@ -1,12 +1,27 @@
+import sys
+from pathlib import Path
+
+# Scenes in subfolders are not on sys.path; add repo root for local imports.
+for _parent in Path(__file__).resolve().parents:
+    if (_parent / "mobjects").is_dir():
+        sys.path.insert(0, str(_parent))
+        break
+
 from manimlib import *
-from manim_slides import Slide
+from mobjects import Table
 import csv
 
 
-class MainScene(Slide):
+class MainScene(Scene):
     def construct(self) -> None:
+
+    #region TITLE SLIDE
+        datatable = []
         with open(r"stats-final-project\iPhone vs Android Bias Survey.csv", mode='r') as file:
             reader = csv.reader(file)
+            for row in reader:
+                datatable.append(row)
+            
 
         #create apple logo
         apple_logo = SVGMobject(r"assets\svg\Apple_logo_black.svg")
@@ -35,6 +50,111 @@ class MainScene(Slide):
         author_text.shift(DOWN)
         author_text.shift(RIGHT *1.8)
         self.play(ShowCreation(author_text))
+
+        tofade = [apple_logo, android_logo, vs_text, title_text, author_text]
+        fadegroup = VGroup()
+        for item in tofade:
+            fadegroup.add(item)
+
+        self.play(FadeOut(fadegroup))
+    #endregion
+
+
+    #region RESEARCH QUESTION
+        q_text_group = VGroup()
+
+        q_text_1 = Text("I've always been interested in this. I've heard\nthat people are really attatched to\ntheir iPhones, but that usually comes from social media.")
+        q_text_2 = Text("I wanted to look at how it might show up in the real world.")
+        q_text_group.add(q_text_1, q_text_2)
+        q_text_group.scale(0.75)
+        q_text_1.shift(LEFT * 0.33)
+        q_text_2.shift(DOWN)
+        q_text_group.align_on_border(LEFT)
+
+
+        self.play(ShowCreation(q_text_1))
+        self.play(ShowCreation(q_text_2))
+
+        self.play(FadeOut(q_text_group))
+
+        """
+        knowing that I wanted to look at this, I found a survay talking about this bias in adults,
+        and I asked the same questions to the NWS. Now, I'll be able to compare our answers to see if there
+        is a significant difference
+        """
+
+        ex_text_group = VGroup()
+        ex_text_1 = Text("Knowing that I wanted to look at this bias, I found a survay talking about it in adults,")
+        ex_text_2 = Text("and asked the same questions to the NWS population. Now, I'll be able to compare the results from NWS")
+        ex_text_3 = Text("and see if we have a significant difference from the survay.")
+        ex_text_group.add(ex_text_2, ex_text_1, ex_text_3)
+
+        self.play(ShowCreation(ex_text_1))
+        self.play(ShowCreation(ex_text_2))
+        self.play(ShowCreation(ex_text_3))
+
+        self.play(FadeOut(ex_text_group))
+
+        claim_table = Table(
+            [
+                ["Claim", "Survay %"],
+                ["Android users have been excluded from group chats", "24%"],
+                ["Android users have been made fun of for their phone type", "52%"],
+                ["iPhone users think less of Android users because of their phone", "22%"],
+                ["Android users consider switching to iPhone because of peer presure", "30%"],
+                ["iPhone users will switch to another messaging app to accomidate Andriod users", "42%"]
+            ],
+            header_row=True,
+            full_width=True,
+            column_width_ratios=(4, 1),
+        )
+
+        claim_table.scale(0.5)
+        claim_table.align_on_border(LEFT)
+        self.play(ShowCreation(claim_table))
+    #endregion
+
+
+    #region HYPOTHESIS
+        """
+        Because there are 5 claims, I actually have 5 seperate hypothesis tests to run
+        """
+        test_text_group = VGroup()
+        test_text_1 = Text("Because there are 5 different claims, I actaully\nhave 5 seperate hypothesis tests to run.")
+        test_text_2 = Text("Because of what I hear around school, all hypothosis\nwill be that there is more bias at NWS than in the survay.")
+        test_text_3 = Text("Hypothosis (it's the same for all claims):")
+        test_text_4 = Tex(r"H_{0}:\widehat{p}_{1}-\widehat{p}_{2}=0")
+        test_text_5 = Tex(r"H_{A}:\widehat{p}_{1}-\widehat{p}_{2}>0")
+        test_text_6 = Tex(r"\text{Where }\widehat{p}_{1} \text{ is the proportion from NWS and }\widehat{p}_{2}\text{ is the proportion from the survay.}")
+        test_text_group.add(test_text_1, test_text_2, test_text_3, test_text_4, test_text_5, test_text_6)
+        test_text_group.scale(0.5)
+        test_text_group.align_to(claim_table,RIGHT)
+
+        self.play(ShowCreation(test_text_group))
+
+
+
+    #endregion
+
+
+    #region DATA
+
+    #endregion
+
+
+    #region DISPLAY
+
+    #endregion
+
+
+    #region 95 CONFIDENCE
+
+    #endregion
+
+
+    #region T-TEST
+
+    #endregion
 
         self.embed()
 
